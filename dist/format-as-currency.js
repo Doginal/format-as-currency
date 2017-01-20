97d5e26ac4743284d595318918b0c96a7148e41f
Doginal <alex.griggs@gmail.com>
2017-01-20 00:37:29 -0800

    updated dist/

--- dist/format-as-currency.js
+++ dist/format-as-currency.js
@@ -1,4 +1,4 @@
-!function(e){"object"==typeof exports?module.exports=e():"function"==typeof define&&define.amd?define(e):"undefined"!=typeof window?window.formatAsCurrency=e():"undefined"!=typeof global?global.formatAsCurrency=e():"undefined"!=typeof self&&(self.formatAsCurrency=e())}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.formatAsCurrency = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 // for usage via require()
 module.exports = 'bcherny/formatAsCurrency'
 
@@ -52,6 +52,22 @@ angular
 
     return parseFloat(currencyString.replace(/(\$|\,)+/g, ''), 10)
   }
+  
+  // (inputValue: number, minValue: number) => true/false
+  // eg. (0.1,1) => false
+  // eg. (1, 1) => true
+  this.minValidator = function(inputValue, minValue) {
+    if(!minValue) return true;
+    if(inputValue < minValue) { return false } else { return true }
+  }
+  
+  // (inputValue: number, maxValue: number) => true/false
+  // eg. (300,100) => false
+  // eg. (100,300) => true
+  this.maxValidator = function(inputValue, maxValue) {
+    if(!maxValue) return true; 
+    if(inputValue <= maxValue) { return true } else { return false }
+  }
 
   // (array: Array) => Array
   // eg. [1,2,2] => [1,2]
@@ -90,6 +106,10 @@ angular
   return {
     require: 'ngModel',
     restrict: 'A',
+    scope: {
+      facMin: '=',
+      facMax: '='
+    },
     link: function (scope, element, attrs, ngModel) {
 
       var filter = $filter('currency')
@@ -151,6 +171,14 @@ angular
         return !isNaN(modelValue)
       }
 
+      ngModel.$validators.min = function (modelValue) {
+        return util.minValidator(modelValue, scope.facMin);
+      }
+
+      ngModel.$validators.max = function (modelValue) {
+        return util.maxValidator(modelValue, scope.facMax);
+      }
+
       // manually trigger the $formatters pipeline
       function triggerRender() {
         ngModel.$setViewValue(ngModel.$formatters.reduce(function (value, fn) {
@@ -162,6 +190,5 @@ angular
   }
 
 }])
-},{}]},{},[1])
-(1)
+},{}]},{},[1])(1)
 });
